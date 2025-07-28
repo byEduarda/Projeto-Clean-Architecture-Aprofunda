00340980126f0d8d7713e06769ee1d3f5cec95c4
byEduarda <albuquerque.zxl@gmail.com>
2025-07-28 18:23:03 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
 import express, { Application } from 'express';
-import cors from 'cors'; 
-import libraryRouter from './routes/libraryRouter';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { authMiddleware } from './shared/middlewares/authMiddleware';
+import authRoutes from './routes/authRoutes';
+import libraryRoutes from './routes/libraryRoutes';
+
+dotenv.config();
 
 const app: Application = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.use(express.json()); 
+if (!MONGO_URI) {
+  throw new Error('A variável MONGO_URI não está definida no .env');
+}
+
+app.use(express.json());
 app.use(cors());
 
-app.use(libraryRouter);
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('🟢 Conectado ao MongoDB Atlas com sucesso');
 
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor rodando com sucesso em http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar ao MongoDB:', error);
+  });
+
+
+app.use('/auth', authRoutes);;
+app.use('/', libraryRoutes);
 
 export default app;
